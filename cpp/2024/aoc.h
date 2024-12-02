2c0b2a1bcc0d54bb6fdb4837896b73a6235897ea
nyxbird <65233477+nyxbird@users.noreply.github.com>
2024-12-02 16:29:15 -0500

    day 2!

---
+++ cpp/2024/aoc.h
@@ -0,0 +1,20 @@
+#include <bits\stdc++.h>
+using namespace std;
+typedef vector<int> vn;
+typedef vector<vn> vvn;
+typedef vector<vvn> vvvn;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vvl> vvvl;
+
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef pair<int, int> pnn;
+
+#define forr(i,n) for(int i=0;i<n;++i)
+
+#define test(n, p) int main(){fstream f("in/"#n".txt"); cout<<part##n(f);}
+#define solve(n) int main(){fstream f("in/"#n".txt");printf("1: %l\n2:%l", part1(f), part2(f));}
